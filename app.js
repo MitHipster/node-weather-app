e7e571b8467d8b293eefb6e675ed675359ad84ba
Tim Acker <tjacker31@gmail.com>
2019-02-08 20:17:13 -0500

    Incorporates yargs into project to handle and define address flag

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const dotenv = require('dotenv');
 const request = require('request');
+const yargs = require('yargs');
 const apiKey = dotenv.config().parsed.API_KEY;
+
+const argv = yargs
+	.options({
+		a: {
+			demand: true,
+			alias: 'address',
+			describe: 'Address for requested weather information',
+			string: true // parses user input as a string
+		}
+	})
+	.help()
+	.alias('help', 'h').argv;
+console.log('==>: argv', argv);
+
+const encodedLocation = encodeURIComponent(argv.a);
 
 request(
 	{
 		url:
-			'http://www.mapquestapi.com/geocoding/v1/address?key=' +
+			'http://www.mapquestapi.com/geocoding/v1/address' +
+			'?key=' +
 			apiKey +
-			'&location=1301%20lombard%20street%20philadelphia',
+			'&location=' +
+			encodedLocation,
 		json: true
 	},
 	(error, response, body) => {
-		console.log('==>: body', body);
+		console.log(
+			JSON.stringify(body.results[0].locations[0].latLng.lat, null, 2)
+		);
 	}
 );
