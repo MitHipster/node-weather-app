3489f4a89e028f054336a387b2de06017a31f634
Tim Acker <tjacker31@gmail.com>
2019-02-09 22:28:30 -0500

    Removes native promises and uses the axios library

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const dotenv = require('dotenv');
+const mapKey = dotenv.config().parsed.MAP_API_KEY;
+const weatherKey = dotenv.config().parsed.WEATHER_API_KEY;
 const yargs = require('yargs');
+const axios = require('axios');
 const chalk = require('chalk');
-
-const geocode = require('./geocode/geocode');
-const weather = require('./weather/weather');
 
 const argv = yargs
 	.options({
@@ -16,20 +17,38 @@ const argv = yargs
 	.help()
 	.alias('help', 'h').argv;
 
-geocode
-	.geocodeAddress(argv.address)
-	.then(address => {
+const encodedLocation = encodeURIComponent(argv.address);
+const geocodeURL = `http://www.mapquestapi.com/geocoding/v1/address?key=${mapKey}&location=${encodedLocation}`;
+
+axios
+	.get(geocodeURL)
+	.then(response => {
+		const statusCode = response.data.info.statuscode;
+		const location = response.data.results[0].locations[0] || {};
+
+		if (statusCode === 400) throw new Error('\nInvalid input. Please try again.\n');
+
 		console.info(
 			chalk.blue('\nLocation:'),
-			`${address.street} ${address.city}, ${address.state} ${address.postalCode}\n`
+			`${location.street} ${location.adminArea5}, ${location.adminArea3} ${location.postalCode}\n`
 		);
 
-		return weather.getWeather(address.lat, address.lng);
+		const lat = location.latLng.lat;
+		const lng = location.latLng.lng;
+		const weatherURL = `https://api.darksky.net/forecast/${weatherKey}/${lat},${lng}`;
+
+		return axios.get(weatherURL);
 	})
-	.then(weather => {
+	.then(response => {
+		const weather = response.data.currently || {};
+
 		console.info(chalk.blue('Temperature:'), weather.temperature);
-		console.info(chalk.blue('Feels Like:'), weather.feelsLike);
+		console.info(chalk.blue('Feels Like:'), weather.apparentTemperature);
 	})
-	.catch(err => {
-		console.warn(chalk.red(err));
+	.catch(error => {
+		if (error.code === 'ENOTFOUND') {
+			console.warn(chalk.red('\nUnable to connect to servers. Please try again later.\n'));
+		} else {
+			console.warn(chalk.red('\n' + error.message + '\n'));
+		}
 	});
