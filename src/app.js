eb8b487b8fd2a7a4c8c59d85145961ee43f5217b
Tim Acker <tjacker31@gmail.com>
2019-03-10 21:33:11 -0400

    Customizes hbs folder locations in express

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+// Configure handlebars templating engine and define
+// a custom handlebars directory versus the views default
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../templates/views'));
+hbs.registerPartials(path.join(__dirname, '../templates/partials'));
 
 // Set location of static assets. This is default to serving
 // index.html at the root url
@@ -11,7 +16,22 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
 	res.render('index', {
-		title: 'Node Weather Application'
+		title: 'Node Weather Home Page',
+		name: 'Tim Acker'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('index', {
+		title: 'Node Weather About Page',
+		name: 'Tim Acker'
+	});
+});
+
+app.get('/help', (req, res) => {
+	res.render('index', {
+		title: 'Node Weather Help Page',
+		name: 'Tim Acker'
 	});
 });
 
