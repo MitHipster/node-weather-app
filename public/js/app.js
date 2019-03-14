641c4f2a33c2ad18fc88f3ac70f6dfbcbfabf71a
Tim Acker <tjacker31@gmail.com>
2019-03-13 22:12:18 -0400

    Adds form to enter and submit location to fetch method

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,21 @@
-/*global fetch*/
+const searchForm = document.querySelector('form');
+const searchInput = document.querySelector('input');
 
-console.info('Client side JavaScript file is loaded.');
+searchForm.addEventListener('submit', e => {
+	e.preventDefault();
 
-fetch('http://localhost:3000/weather?location=boston').then(resp => {
-	resp.json().then(data => {
-		if (data.error) {
-			console.warn(data.error.message);
-		} else {
-			console.info(data.location);
-		}
+	const location = searchInput.value;
+	if (!location) return;
+
+	fetch(`/weather?location=${location}`).then(resp => {
+		resp.json().then(data => {
+			if (data.error) {
+				console.warn(data.error.message);
+			} else {
+				console.info(data.location);
+			}
+		});
 	});
+
+	searchInput.value = '';
 });
